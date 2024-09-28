b54958f94a78b515f17bb2412f0e5282cfe05f7a
Syed Javeed <syedjaveed@nextgen-is.com>
2024-09-29 00:33:00 +0530

    small changes in config: package.json, env types

--- api/src/types/env.d.ts
+++ api/src/types/env.d.ts
@@ -1,10 +1,6 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    DB_HOST: string;
-    DB_PORT: string;
-    DB_USERNAME: string;
-    DB_PASSWORD: string;
-    DB_DATABASE: string;
+    MONGO_URL: string;
     JWT_SECRET: string;
   }
 }
